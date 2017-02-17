7c60f0e7d06d86beb10e2bcfafe141e562c8ef48
Sudarshan Panchal <sudarshan_panchal@persistent.co.in>
2017-02-17 14:39:58 +0530

    third

---
+++ static/js/Script.js
@@ -0,0 +1,52 @@
+$(function(){
+
+	var sentHead = "<div class='row' style='margin: 5px 0px'> <div class='col-sm-offset-4 col-sm-8 text-right'> <div class='sent text-left'>";
+	var receivedHead = "<div class='row' style='margin: 5px 0px'> <div class='col-sm-8 text-left'> <div class='received text-left'>";
+	var tail = "</div> </div> </div>";
+		
+	function send() {
+		var data = {
+			"message" : $("#message").val(),
+			
+		};
+
+		$(sentHead+data.message+tail).hide().appendTo('.chatdiv').show("puff", {times : 3}, 200);
+
+		$(".chatdiv").animate({ scrollTop: $('.chatdiv').prop("scrollHeight")}, 1000);
+		$("#message").val("");
+
+		$.post("/",data,function(res){
+			console.log(res)
+
+
+			$(receivedHead+res+tail).hide().appendTo('.chatdiv').show("puff", {times : 3}, 200);
+
+			$(".chatdiv").animate({ scrollTop: $('.chatdiv').prop("scrollHeight")}, 1000);
+
+			});
+
+	};
+
+
+	$("#message").keypress(function(event) {
+		if (event.which == 13) {
+			event.preventDefault();
+
+			if( $("#message").val() != "")
+				send();
+		}
+	});
+
+	$("#send").click(function(event){
+		if( $("#message").val() != "")
+			send()
+	})
+
+	function setInput(text) {
+		$("#message").val(text);
+		//send()
+	}
+});
+
+
+
