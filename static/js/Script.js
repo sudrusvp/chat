3f67a5b0505192143daca48561cc92b6106eaf1c
Sudarshan Panchal <sudarshan_panchal@persistent.co.in>
2017-03-01 11:29:22 +0530

    sixth

--- static/js/Script.js
+++ static/js/Script.js
@@ -5,20 +5,21 @@ $(function(){
 	var tail = "</div> </div> </div>";
 		
 	function send() {
-		console.log("inside send")
+		console.log("inside send1")
 		var data = {
 			"message" : $("#message").val(),
 			
 		};
-
+		console.log("inside send2")
 		$(sentHead+data.message+tail).hide().appendTo('.chatdiv').show("puff", {times : 3}, 200);
-
+		console.log("inside send3")
 		$(".chatdiv").animate({ scrollTop: $('.chatdiv').prop("scrollHeight")}, 1000);
+		console.log("inside send4")
 		$("#message").val("");
-		
+		console.log("inside send5")
 		console.log("calling..")
 		$.post("/",data,function(res){
-			console.log("inside response")
+			console.log("inside post1")
 			console.log(res)
 
 
