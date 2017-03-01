c419661c0f261e2cf14e7c9f81bc36850b587c4d
Sudarshan Panchal <sudarshan_panchal@persistent.co.in>
2017-03-01 14:25:21 +0530

    sixth

--- static/js/Script.js
+++ static/js/Script.js
@@ -10,7 +10,7 @@ $(function(){
 			"message" : $("#message").val(),
 			
 		};
-		console.log("inside send2")
+		//console.log("inside send2")
 		$(sentHead+data.message+tail).hide().appendTo('.chatdiv').show("puff", {times : 3}, 200);
 		//console.log("inside send3")
 		$(".chatdiv").animate({ scrollTop: $('.chatdiv').prop("scrollHeight")}, 1000);
